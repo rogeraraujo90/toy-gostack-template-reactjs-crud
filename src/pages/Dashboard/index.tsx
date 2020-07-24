fce6ee1b4c4d82160f500b067b8a2eb29d12d6ba
Roger Araujo <rogeralves_90@yahoo.com.br>
2020-07-24 19:50:55 -0300

    Finishing features

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -63,7 +63,9 @@ const Dashboard: React.FC = () => {
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+    const newFoods = foods.filter(food => food.id !== id);
+    setFoods(newFoods);
   }
 
   function toggleModal(): void {
